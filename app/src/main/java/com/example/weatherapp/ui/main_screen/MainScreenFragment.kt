d31ae00351e5b49cc7ca96d1316f6edd5a9a1150
ilya <ruilyakiselev@gmail.com>
2021-10-06 12:51:54 +0700

    add RxBinding for search cities

--- app/src/main/java/com/example/weatherapp/ui/main_screen/MainScreenFragment.kt
+++ app/src/main/java/com/example/weatherapp/ui/main_screen/MainScreenFragment.kt
@@ -1,16 +1,21 @@
 package com.example.weatherapp.ui.main_screen
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.widget.addTextChangedListener
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.example.weatherapp.R
 import com.example.weatherapp.databinding.FragmentMainScreenBinding
+import com.jakewharton.rxbinding4.widget.textChangeEvents
 import dagger.hilt.android.AndroidEntryPoint
-
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.schedulers.Schedulers
+import java.util.concurrent.TimeUnit
 
 @AndroidEntryPoint
 class MainScreenFragment : Fragment() {
@@ -32,23 +37,23 @@ class MainScreenFragment : Fragment() {
             false
         )
 
+        binding.lifecycleOwner = this
+
         val view: View = binding.root
 
-        binding.button.setOnClickListener {
-            viewModel.loadForecastByCityName(binding.cityNameEditText.text.toString())
-        }
+        binding.viewModel = viewModel
+
+
+        binding.cityNameEditText.textChangeEvents()
+            .debounce(500, TimeUnit.MILLISECONDS)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({
+                viewModel.loadForecastByCityName(it.text.toString())
+            }, {
+                Log.e("MyLog","CoroutineExceptionHandler got ${it.localizedMessage}")
+            })
 
         return view
-    }
-
-    override fun onStart() {
-        super.onStart()
-
-        viewModel.trackedForecast.observe(viewLifecycleOwner) {
-            binding.weatherResponseText = it.toString()
-        }
-
-        viewModel.loadForecastByCityName("Novosibirsk")
-        viewModel.loadForecastByGeographicCoordinates(35, 139)
     }
 }
